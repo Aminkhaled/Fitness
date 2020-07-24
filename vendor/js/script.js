4d7602ca06cb7c8369ec3eddce464bf936665c78
Amen Khaled <Aminkhaled@users.noreply.github.com>
2020-07-24 12:14:34 +0200

    we added more section Our Health Care Packages

--- vendor/js/script.js
+++ vendor/js/script.js
@@ -1,8 +1,29 @@
 // interval is in milliseconds. 1000 = 1 second -> so 1000 * 10 = 10 seconds
-$('.carousel').carousel({
-    interval: 500 * 10
-});
 
-setTimeout(function(){
-    $('.loader').css('display', 'none');
-}, 4000);
+$(document).ready(function() {
+    $('.carousel').carousel({
+        interval: 500 * 10
+    });
+
+    setTimeout(function(){
+        $('.loader').css('display', 'none');
+    }, 4000);
+    $(window).scroll(function () {
+        console.log($(window).scrollTop())
+        if ($(window).scrollTop() > 280) {
+            $('.navbar').addClass('navbar-fixed');
+            $('.navbar-brand').addClass('fixed-brand');
+            $('.nav-nav-light').css('background-color','rgba(1,2,3,0.58)');
+
+        }
+        if ($(window).scrollTop() < 281) {
+            $('.navbar').removeClass('navbar-fixed');
+            $('.navbar-brand').removeClass('fixed-brand');
+
+        }
+    });
+    // optional
+    $('#blogCarousel').carousel({
+        interval: 9000
+    });
+});
